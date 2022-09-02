7601206f3cea282ee6a639a2cba228fe9e62e8dc
Giordano Cicchetti <giordano.cicchetti.gc@gmail.com>
2022-09-02 17:51:13 +0200

    navbar

--- auth/config/checkAuth.js
+++ auth/config/checkAuth.js
@@ -21,6 +21,24 @@ module.exports = {
         }
         req.flash('error_msg', 'You are not allowed to do this action!');
         res.redirect('/auth/login');
+    },
+    ensureManager: function (req, res, next) {
+        if (req.isAuthenticated()) {
+            if(req.user.role=="Manager"){
+                return next();
+            }
+        }
+        req.flash('error_msg', 'You are not allowed to do this action!');
+        res.redirect('/auth/login');
+    },
+    ensureUser: function (req, res, next) {
+        if (req.isAuthenticated()) {
+            if(req.user.role=="User"){
+                return next();
+            }
+        }
+        req.flash('error_msg', 'You are not allowed to do this action!');
+        res.redirect('/auth/login');
     }
 
 };
--- auth/routes/index.js
+++ auth/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/checkAuth')
-
+const { ensureAdmin } = require('../config/checkAuth')
+const { ensureManager} = require('../config/checkAuth')
+const { ensureUser } = require('../config/checkAuth')
 const authController = require('../controllers/authController')
 const { authorize } = require('../config/authorize')
 
@@ -11,16 +13,26 @@ router.get('/', (req, res) => {
     res.render('welcome');
 });
 
+router.get('/navbar', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('navbar',{name: req.user.name});
+    }
+    else{ 
+        res.render('navbar');
+    }
+    
+});
+
 //------------ Dashboard Route ------------//
-router.get('/profiloUser', ensureAuthenticated, (req, res) => res.render('profiloUser', {
+router.get('/profiloUser', ensureUser ,  (req, res) => res.render('profiloUser', {
     name: req.user.name
 }));
 
-router.get('/profiloAdmin', ensureAuthenticated, (req, res) => res.render('profiloAdmin', {
+router.get('/profiloAdmin', ensureAdmin, (req, res) => res.render('profiloAdmin', {
     name: req.user.name
 }));
 
-router.get('/profiloManager', ensureAuthenticated, (req, res) => res.render('profiloManager', {
+router.get('/profiloManager', ensureManager, (req, res) => res.render('profiloManager', {
     name: req.user.name
 }));
 
@@ -31,6 +43,7 @@ router.get('/authorizeUser', (req, res) => authorize(Role.User,req,res));
 router.get('/authorizeManager', (req, res) => authorize(Role.Manager,req,res));
 
 router.get('/authorizeAdmin', (req, res) => authorize(Role.Admin,req,res));
+
 
 
 module.exports = router;
