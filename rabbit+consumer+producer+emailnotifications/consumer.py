359820002fd6e29c536184fcb9fa60f665978aba
Giordano Cicchetti <giordano.cicchetti.gc@gmail.com>
2022-09-06 22:49:52 +0200

    schema_rabbitmq

--- rabbit+consumer+producer+emailnotifications/consumer.py
+++ rabbit+consumer+producer+emailnotifications/consumer.py
@@ -1,10 +1,8 @@
-import pika
-import sendgrid
+# example_consumer.py
 import os
 import subprocess
 import sys
 os.environ['SENDGRID_API_KEY'] = "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"
-
 
 import pika, os, time
 import requests
@@ -12,11 +10,10 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 from sendgrid import SendGridAPIClient
-
-
+  
+import pymongo
 
 def send_simple_message(user,msg):
-
   message = Mail(
       from_email=user,
       to_emails=user,
@@ -32,19 +29,25 @@ def send_simple_message(user,msg):
       print(response.headers)
   except Exception as e:
       print(e.message)
-
-
 def pdf_process_function(msg):
   print(" PDF processing")
   msg=str(msg,'utf-8')
   print(" [x] Received " + msg)
-  #utenti query 
+  #connetti al db
+  #utenti query che interessa msg
+  #myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+  #mydb = myclient["mydatabase"]
+  #mycol = mydb["customers"]
+  #myquery = { "address": "Park Lane 38" }
+  #mydoc = mycol.find(myquery)
+  #for x in mydoc:
+    #print(x) # send
   user="carmignanifederico@gmail.com"
   send_simple_message(user,msg)
   print("email sent!")
   time.sleep(5) # delays for 5 seconds
-  print(" PDF processing finished");
-  return;
+  print(" PDF processing finished")
+  return
 
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
 url = os.environ.get('CLOUDAMQP_URL', 'amqps://tldmywiz:fSr5JmVpRN1mnUSmPjByc6RbVTAAZwDv@rat.rmq2.cloudamqp.com/tldmywiz')
@@ -58,11 +61,9 @@ channel.queue_declare(queue='cinema')
 channel.queue_declare(queue='teatro')
 channel.queue_declare(queue='discoteca')
 # Declare a queue
-
 # create a function which is called on incoming messages
 def callback(ch, method, properties, body):
   pdf_process_function(body)
-
 # set up subscription on the queue
 channel.basic_consume('cultura',
   callback,
